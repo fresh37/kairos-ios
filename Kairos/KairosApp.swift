6fcc701e5d1fa585cd0d0bf5c18186a68b4d0506
fresh37 <fishinator9000@gmail.com>
2026-03-23 20:47:49 -0400

    Initial Commit

---
+++ Kairos/KairosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KairosApp.swift
+//  Kairos
+//
+//  Created by Kevin on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct KairosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
